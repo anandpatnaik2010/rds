474089d1044c8291ba1f7460d4d78eb69584e73a
subhampanda <panda.subham4@gmail.com>
2022-02-14 01:32:42 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 resource "aws_db_snapshot" "subham-createSnapshot" {
-#db_instance_identifier = "${var.db_instance_identifier}"
-db_snapshot_identifier = "${var.db_snapshot_identifier}"
+db_instance_identifier = "${var.db_instance_identifier}"
+#db_snapshot_identifier = "${var.db_snapshot_identifier}"
 }
 
