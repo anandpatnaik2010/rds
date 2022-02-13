29353f6770d2ac6805d17313c879912203c172c3
subhampanda <panda.subham4@gmail.com>
2022-02-13 16:51:57 +0530

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,15 +1,4 @@
-data "aws_db_snapshot" "db_snapshot" {
-    most_recent = true
-    db_instance_identifier = "db-prod"
-}
-# Create new staging DB
-resource "aws_db_instance" "db_uat" {
-  instance_class       = "db.t2.micro"
-  identifier           = "db-uat"
-  username             = "subham"
-  password             = "subham123"
-
-  snapshot_identifier  = "${data.aws_db_snapshot.db_snapshot.id}"
-
-  skip_final_snapshot = true
+resource "aws_db_snapshot" "lm-mmx-createSnapshot" {
+db_instance_identifier = ""
+db_snapshot_identifier = "${var.db_snapshot_identifier}"
 }
