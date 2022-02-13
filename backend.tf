12a4f1b3eb27dddc7dfa2b1595e72c975d0645d9
subhampanda <panda.subham4@gmail.com>
2022-02-14 03:41:14 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,3 +3,9 @@ db_instance_identifier = "${var.db_instance_identifier}"
 db_snapshot_identifier = "${var.db_snapshot_identifier}"
 }
 
+resource "aws_db_instance" "subham-restoreDB" {
+depends_on = [aws_db_snapshot.subham-createSnapshot]
+snapshot_identifier = "${var.db_snapshot_identifier}"
+skip_final_snapshot = true
+
+}
