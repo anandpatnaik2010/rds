78a0d18340520954210c135a6c7a413a559b5643
anandpatnaik2010 <47821007+anandpatnaik2010@users.noreply.github.com>
2022-02-21 15:20:42 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,23 @@
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
 resource "aws_db_snapshot" "subham-createSnapshot" {
 db_instance_identifier = "${var.db_instance_identifier}"
 db_snapshot_identifier = "${var.db_snapshot_identifier}"
